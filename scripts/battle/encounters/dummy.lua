9d5234e15876d9fc588cd928e6cb0692437c4c72
Agent 7 <ronaldmclamb@gmail.com>
2024-05-09 14:35:39 -0400

    bomb enemy wip

--- scripts/battle/encounters/dummy.lua
+++ scripts/battle/encounters/dummy.lua
@@ -7,7 +7,7 @@ function Dummy:init()
     self.text = "* Defuse the dummy...?"
 
     -- Battle music ("battle" is rude buster)
-    self.music = "battle"
+    self.music = "mus_news_battle"
     -- Enables the purple grid battle background
     self.background = true
 
@@ -16,6 +16,21 @@ function Dummy:init()
 
     --- Uncomment this line to add another!
     --self:addEnemy("dummy")
+
+    self.defuser = DefuseZone(500, 128, 100, 50)
+    Game.battle:addChild(self.defuser)
+    self.siner = 0
+end
+
+function Dummy:update()
+end
+
+function Dummy:beforeStateChange(old, new)
+    if new == "VICTORY" then
+        Game.battle:setState("TRANSITIONOUT")
+    elseif new == "DEFENDINGBEGIN" then
+        Game.battle:setState("ACTIONSELECT")
+    end
 end
 
 return Dummy
--- scripts/battle/enemies/dummy.lua
+++ scripts/battle/enemies/dummy.lua
@@ -4,7 +4,7 @@ function Dummy:init()
     super.init(self)
 
     -- Enemy name
-    self.name = "Dummy"
+    self.name = "Dummy Bomb"
     -- Sets the actor, which handles the enemy's sprites (see scripts/data/actors/dummy.lua)
     self:setActor("dummy")
 
@@ -19,78 +19,115 @@ function Dummy:init()
     self.money = 100
 
     -- Mercy given when sparing this enemy before its spareable (20% for basic enemies)
-    self.spare_points = 20
+    self.spare_points = 0
 
     -- List of possible wave ids, randomly picked each turn
     self.waves = {
-        "basic",
-        "aiming",
-        "movingarena"
+        --"basic",
+        --"aiming",
+        --"movingarena"
     }
 
     -- Dialogue randomly displayed in the enemy's speech bubble
     self.dialogue = {
-        "..."
+        "[wave:3][speed:0.5]Tick... ",
+        "[wave:3][speed:0.5]Tock... "
     }
 
     -- Check text (automatically has "ENEMY NAME - " at the start)
-    self.check = "AT 4 DF 0\n* Cotton heart and button eye\n* Looks just like a fluffy guy."
+    self.check = "Despite the name, this one is not a dud."
 
     -- Text randomly displayed at the bottom of the screen each turn
     self.text = {
-        "* The dummy gives you a soft\nsmile.",
-        "* The power of fluffy boys is\nin the air.",
-        "* Smells like cardboard.",
+        "* The bomb is still active!",
     }
     -- Text displayed at the bottom of the screen when the enemy has low health
     self.low_health_text = "* The dummy looks like it's\nabout to fall over."
 
-    -- Register act called "Smile"
-    self:registerAct("Smile")
-    -- Register party act with Ralsei called "Tell Story"
-    -- (second argument is description, usually empty)
-    self:registerAct("Tell Story", "", {"ralsei"})
+    self:registerAct("Defuse Bomb", "Aim for\ndefuse\nzone!", "all")
+
+    -- Hitbox used for bomb purposes
+    self:setHitbox(0,0,self.width,self.height)
+    
+    self.siner = 0
+
+end
+
+function Dummy:update()
+    super.update(self)
+    -- Once the battle has properly started, this enemy will move up and down in a sine motion.
+    if Game.battle.state ~= "TRANSITION" and Game.battle.state ~= "INTRO" and not self.defused and not self.lock_movement then
+        if not self.start_x then
+            self.start_x = self.x
+            self.start_y = self.y
+        end
+        self.siner = self.siner + DT
+        self.y = self.start_y + math.sin(self.siner*2) * 120
+    end    
 end
 
 function Dummy:onAct(battler, name)
-    if name == "Smile" then
-        -- Give the enemy 100% mercy
-        self:addMercy(100)
-        -- Change this enemy's dialogue for 1 turn
-        self.dialogue_override = "... ^^"
-        -- Act text (since it's a list, multiple textboxes)
-        return {
-            "* You smile.[wait:5]\n* The dummy smiles back.",
-            "* It seems the dummy just wanted\nto see you happy."
-        }
-
-    elseif name == "Tell Story" then
-        -- Loop through all enemies
-        for _, enemy in ipairs(Game.battle.enemies) do
-            -- Make the enemy tired
-            enemy:setTired(true)
+    if name == "Defuse Bomb" then
+        local result
+        for i, defuser in ipairs(Game.world.stage:getObjects(DefuseZone)) do
+            result = defuser:defuse(self)
+            if result == true then break end
         end
-        return "* You and Ralsei told the dummy\na bedtime story.\n* The enemies became [color:blue]TIRED[color:reset]..."
-
-    elseif name == "Standard" then --X-Action
-        -- Give the enemy 50% mercy
-        self:addMercy(50)
-        if battler.chara.id == "ralsei" then
-            -- R-Action text
-            return "* Ralsei bowed politely.\n* The dummy spiritually bowed\nin return."
-        elseif battler.chara.id == "susie" then
-            -- S-Action: start a cutscene (see scripts/battle/cutscenes/dummy.lua)
-            Game.battle:startActCutscene("dummy", "susie_punch")
-            return
+        self:onDefuse(result)
+        if result == true then
+            Assets.playSound("break2")
+            return "[noskip]* Bomb defused![wait:1s]"
         else
-            -- Text for any other character (like Noelle)
-            return "* "..battler.chara:getName().." straightened the\ndummy's hat."
+            Assets.playSound("break1")
+            return "* Defuse failed![wait:5]\n* Aim for DEFUSE ZONE!"
         end
+    elseif name == "Standard" then --X-Action
+        return "* "..battler.chara:getName().." tried something.[wait:10] Nothing happened."
     end
 
     -- If the act is none of the above, run the base onAct function
     -- (this handles the Check act)
     return super.onAct(self, battler, name)
 end
+
+function Dummy:onDefuse(success)
+    if success then
+        self.defused = true
+        self:statusMessage("msg", "defuse")
+        self.color = COLORS.lime
+        Game.battle.timer:after(0.5, function ()
+            self:spare()
+        end)
+    else
+        self:statusMessage("msg", "miss")
+        self:mercyFlash()
+    end
+end
+
+function Dummy:hurt(amount, battler, on_defeat, color, show_status, attacked)
+    if amount == 0 or (amount < 0 and Game:getConfig("damageUnderflowFix")) then
+        if show_status ~= false then
+            self:statusMessage("msg", "miss", color or (battler and {battler.chara:getDamageColor()}))
+        end
+
+        self:onDodge(battler, attacked)
+        return
+    end
+
+    -- Don't take damage
+    --self.health = self.health - amount
+    if show_status ~= false then
+        --self:statusMessage("damage", amount, color or (battler and {battler.chara:getDamageColor()}))
+    end
+
+    if amount > 0 then
+        self.hurt_timer = 1
+        self:onHurt(amount, battler)
+    end
+
+    self:checkHealth(on_defeat, amount, battler)
+end
+
+
 
 return Dummy
